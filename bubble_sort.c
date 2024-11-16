99aa788dc3858e8ef58cb413cc7cb081a86b82d0
Gurmeet Singh Jabbal <gurmeetsinghjabbal9167@gmail.com>
2024-11-16 11:17:47 +0530

    optimized

--- bubble_sort.c
+++ bubble_sort.c
@@ -4,13 +4,16 @@
 void bubbleSort(int arr[], int n) {
     int i, j, temp;
     for (i = 0; i < n - 1; i++) {
+        int flag=1;
         for (j = 0; j < n - i - 1; j++) {
+            flag=0;
             if (arr[j] > arr[j + 1]) {
                 temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
         }
+        if(flag) break;
     }
 }
 // to print the array
